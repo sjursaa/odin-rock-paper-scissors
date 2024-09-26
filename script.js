b0c4607fb8aabd6807213bfdb18bc0b9eb45739c
Sjur <sjursaa@gmail.com>
2024-09-26 14:13:35 +0200

    add playRound function

--- script.js
+++ script.js
@@ -14,11 +14,55 @@ function getComputerChoice() {
 function getHumanChoice() {
   var choice = prompt();
   console.log("You: " + choice);
+  return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("tie");
+    return 0;
+  }
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("you lose");
+    return -1;
+  }
+  if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("you lose");
+    return -1;
+  }
+  if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("you lose");
+    return -1;
+  }
+  if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("you win");
+    return 1;
+  }
+  if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("you win");
+    return 1;
+  }
+  if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("you win");
+    return 1;
+  }
 }
 
 var humanScore = 0;
 var computerScore = 0;
 
-getHumanChoice();
-var one = getComputerChoice();
-console.log("Computer: " + one);
+var humanChoice = getHumanChoice();
+var computerChoice = getComputerChoice();
+console.log("Computer: " + computerChoice);
+
+var result = playRound(humanChoice, computerChoice);
+console.log(result);
+if (result == 1) {
+  humanScore += 1;
+}
+if (result == -1) {
+  computerScore += 1;
+}
+
+console.log("You: " + humanScore);
+console.log("Computer " + computerScore);
