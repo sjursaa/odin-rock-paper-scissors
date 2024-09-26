248d0f8f1ec6e7fcfa03e9058be7306a95efd755
Sjur <sjursaa@gmail.com>
2024-09-26 12:39:58 +0200

    init repo, add index.html, script.js, and add function getComputerChoice

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World, from external script");
+
+console.log("Hello");
+
+function getComputerChoice() {
+  var randomVal = Math.random();
+  if (randomVal < 0.33) {
+    return "paper";
+  }
+  if (randomVal < 0.66) {
+    return "scissors";
+  }
+  if (randomVal < 1) {
+    return "rock";
+  }
+}
+
+var one = getComputerChoice();
+console.log(one);
